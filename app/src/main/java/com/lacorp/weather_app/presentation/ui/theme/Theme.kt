af7e989a349a8c9c8d67698e05ddfc89d9c49761
Lana <chewygum56@gmail.com>
2023-01-30 22:31:38 +0700

    :lipstick: add color and theme

--- app/src/main/java/com/lacorp/weather_app/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/lacorp/weather_app/presentation/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.lacorp.weather_app.presentation.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -16,15 +17,30 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
+)
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val CloudyColorPallete = lightColors(
+    primary = Cloudy50,
+    primaryVariant = Cloudy70,
+    secondary = Cloudy30,
+)
+
+private val SunnyColorPallete = lightColors(
+    primary = Sunny50,
+    primaryVariant = Sunny70,
+    secondary = Sunny30,
+)
+
+private val RainyColorPallete = lightColors(
+    primary = Rainy50,
+    primaryVariant = Rainy70,
+    secondary = Rainy30,
+)
+
+private val StormyColorPallete = lightColors(
+    primary = Stormy50,
+    primaryVariant = Stormy70,
+    secondary = Stormy30,
 )
 
 @Composable
