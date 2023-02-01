a6a5eed620f116429be6d3341b2bbf5b19edd976
Lana <chewygum56@gmail.com>
2023-02-01 17:37:15 +0700

    remove windy weather type

--- app/src/main/java/com/lacorp/weather_app/domain/util/WeatherType.kt
+++ app/src/main/java/com/lacorp/weather_app/domain/util/WeatherType.kt
@@ -7,28 +7,59 @@ sealed class WeatherType(
     val type: String,
     @DrawableRes val icon: Int
 ) {
-    object Sunny: WeatherType(
+    object Sunny : WeatherType(
         "Sunny",
         R.drawable.sunny
     )
 
-    object Cloudy: WeatherType(
-        "Sunny",
+    object Cloudy : WeatherType(
+        "Cloudy",
         R.drawable.cloudy
     )
 
-    object Windy: WeatherType(
-        "Sunny",
-        R.drawable.windy
-    )
-
-    object Rainy: WeatherType(
-        "Sunny",
+    object Rainy : WeatherType(
+        "Rainy",
         R.drawable.rainy
     )
 
-    object Storm: WeatherType(
-        "Sunny",
+    object Stormy : WeatherType(
+        "Stormy",
         R.drawable.storm
     )
+
+    companion object {
+        fun toType(code: Int): WeatherType {
+            return when (code) {
+                0 -> Sunny
+                1 -> Cloudy
+                2 -> Cloudy
+                3 -> Cloudy
+                45 -> Cloudy
+                48 -> Cloudy
+                51 -> Rainy
+                53 -> Rainy
+                55 -> Rainy
+                56 -> Rainy
+                57 -> Rainy
+                61 -> Rainy
+                63 -> Rainy
+                65 -> Rainy
+                66 -> Rainy
+                67 -> Rainy
+                71 -> Rainy
+                73 -> Rainy
+                75 -> Rainy
+                77 -> Rainy
+                80 -> Rainy
+                81 -> Rainy
+                82 -> Rainy
+                85 -> Rainy
+                86 -> Rainy
+                95 -> Stormy
+                96 -> Stormy
+                99 -> Stormy
+                else -> Sunny
+            }
+        }
+    }
 }
