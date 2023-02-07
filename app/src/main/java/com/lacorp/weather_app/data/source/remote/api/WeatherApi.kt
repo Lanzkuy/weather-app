f084bb0bbb5549f38d3754021e016b5ca108d20e
Lana <chewygum56@gmail.com>
2023-02-07 20:23:53 +0700

    add weather enum

--- app/src/main/java/com/lacorp/weather_app/data/source/remote/api/WeatherApi.kt
+++ app/src/main/java/com/lacorp/weather_app/data/source/remote/api/WeatherApi.kt
@@ -3,7 +3,7 @@ package com.lacorp.weather_app.data.source.remote.api
 import com.lacorp.weather_app.data.source.remote.dto.WeatherDailyDto
 import com.lacorp.weather_app.data.source.remote.dto.WeatherHourlyDto
 import retrofit2.http.GET
-import retrofit2.http.Query;
+import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("v1/forecast?hourly=weathercode,temperature_2m,windspeed_10m,pressure_msl,relativehumidity_2m")
---
+++ app/src/main/java/com/lacorp/weather_app/util/WeatherEnum.kt
@@ -0,0 +1,5 @@
+package com.lacorp.weather_app.util
+
+enum class WeatherEnum {
+    CLOUDY, SUNNY, RAINY, STORMY
+}
