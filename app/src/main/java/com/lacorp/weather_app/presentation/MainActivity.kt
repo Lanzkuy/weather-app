dcbe6d9017cd3905b256957a27fb5ab9df5f628d
Lana <59499676+Lanzkuy@users.noreply.github.com>
2023-10-23 12:05:17 +0700

    add function to load current weather, hourly weather, and daily weather

--- app/src/main/java/com/lacorp/weather_app/presentation/MainActivity.kt
+++ app/src/main/java/com/lacorp/weather_app/presentation/MainActivity.kt
@@ -1,118 +1,204 @@
 package com.lacorp.weather_app.presentation
 
+import android.Manifest
+import android.graphics.Color
+import android.location.Geocoder
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.Bottom
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.unit.dp
-import com.lacorp.weather_app.R
+import com.lacorp.weather_app.common.Resource
+import com.lacorp.weather_app.presentation.component.WeatherCurrent
+import com.lacorp.weather_app.presentation.component.WeatherDaily
+import com.lacorp.weather_app.presentation.component.WeatherHourly
+import com.lacorp.weather_app.presentation.theme.Cloudy50
 import com.lacorp.weather_app.presentation.theme.CulturedWhite
 import com.lacorp.weather_app.presentation.theme.WeatherAppTheme
 import com.lacorp.weather_app.util.WeatherEnum
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WeatherApp()
-        }
-    }
-}
-
-@Composable
-fun WeatherApp() {
-    WeatherAppTheme(WeatherEnum.CLOUDY) {
-        val colorPrimary = MaterialTheme.colors.primary
-        val colorPrimaryVariant = MaterialTheme.colors.primaryVariant
-        val colorSecondary = MaterialTheme.colors.secondary
-        val colorSecondaryVariant = MaterialTheme.colors.primaryVariant
-
-        val gradientBackground = listOf(colorPrimary, colorSecondary)
-        val gradientBrush = Brush.verticalGradient(gradientBackground)
-
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(brush = gradientBrush)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 24.dp),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Bottom
-                ) {
-                    Icon(
-                        modifier = Modifier
-                            .size(36.dp)
-                            .align(CenterVertically),
-                        imageVector = Icons.Default.LocationOn,
-                        tint = CulturedWhite,
-                        contentDescription = null,
-                    )
-                    Text(
-                        text = "JAKARTA",
-                        color = CulturedWhite,
-                        style = MaterialTheme.typography.h5,
-                        fontWeight = FontWeight.Light
-                    )
-                }
-                Spacer(modifier = Modifier.padding(top = 32.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Bottom
-                ) {
-                    Icon(
-                        modifier = Modifier
-                            .size(64.dp, 48.dp),
-                        painter = painterResource(id = R.drawable.weather_cloudy),
-                        tint = CulturedWhite,
-                        contentDescription = null,
-                    )
-                    Spacer(modifier = Modifier.padding(4.dp))
-                    Text(
-                        text = "Cloudy",
-                        color = CulturedWhite,
-                        style = MaterialTheme.typography.h5,
-                        fontWeight = FontWeight.Light
-                    )
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
+            val geocoder = Geocoder(application, Locale.getDefault())
+            viewModel.loadWeather(geocoder)
+        }
 
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
+
+        setContent {
+            val weatherState = viewModel.weather.collectAsState().value
+            val location = viewModel.location.collectAsState().value
+            val datetime = viewModel.datetime.collectAsState().value
+
+            when (weatherState) {
+                is Resource.Success -> {
+                    WeatherAppTheme(WeatherEnum from weatherState.data.weatherCurrent?.weatherType?.type.orEmpty()) {
+                        val colorPrimaryVariant = MaterialTheme.colors.primaryVariant
+                        val colorSecondaryVariant = MaterialTheme.colors.secondaryVariant
+
+                        val gradientBackground = Brush.verticalGradient(
+                            0.0f to colorPrimaryVariant,
+                            1.0f to colorSecondaryVariant,
+                            startY = 0.0f,
+                            endY = 2500.0f
+                        )
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(brush = gradientBackground)
+                                .statusBarsPadding()
+                                .navigationBarsPadding()
+                        ) {
+                            Spacer(modifier = Modifier.padding(top = 24.dp))
+                            WeatherCurrent(
+                                modifier = Modifier,
+                                weatherCurrent = weatherState.data.weatherCurrent!!,
+                                location = location,
+                                datetime = datetime
+                            )
+                            Spacer(modifier = Modifier.padding(8.dp))
+                            weatherState.data.weatherHourly!![0].let { data ->
+                                LazyRow(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(
+                                            top = 16.dp,
+                                            start = 24.dp,
+                                            end = 24.dp
+                                        )
+                                        .background(
+                                            colorPrimaryVariant.copy(alpha = 0.25f),
+                                            shape = RoundedCornerShape(12.dp)
+                                        ),
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    content = {
+                                        items(data!!) { hourlyData ->
+                                            WeatherHourly(
+                                                modifier = Modifier,
+                                                painterResourceId = hourlyData.weatherType.icon,
+                                                time = hourlyData.time.toLocalTime().toString(),
+                                                temperature = hourlyData.temperature.toInt()
+                                                    .toString(),
+                                                textStyle = MaterialTheme.typography.body2,
+                                                color = CulturedWhite,
+                                                width = 36.dp,
+                                                height = 36.dp,
+                                                space = 4.dp
+                                            )
+                                        }
+                                    }
+                                )
+                            }
+                            Spacer(modifier = Modifier.padding(8.dp))
+                            weatherState.data.weatherDaily!![0].let { data ->
+                                LazyColumn(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(
+                                            start = 24.dp,
+                                            bottom = 24.dp,
+                                            end = 24.dp
+                                        )
+                                        .background(
+                                            colorPrimaryVariant.copy(alpha = 0.25f),
+                                            shape = RoundedCornerShape(12.dp)
+                                        )
+                                        .weight(1f),
+                                    content = {
+                                        items(data!!) { dailyData ->
+                                            WeatherDaily(
+                                                modifier = Modifier,
+                                                painterResourceId = dailyData.weatherType.icon,
+                                                day = dailyData.time.dayOfWeek.name.lowercase()
+                                                    .replaceFirstChar{ it .uppercase() },
+                                                highTemp = dailyData.maxTemperatures.toInt()
+                                                    .toString(),
+                                                lowTemp = dailyData.minTemperatures.toInt()
+                                                    .toString(),
+                                                textStyle = MaterialTheme.typography.body1,
+                                                color = CulturedWhite,
+                                                width = 32.dp,
+                                                height = 32.dp,
+                                                space = 4.dp
+                                            )
+                                        }
+                                    })
+                            }
+                        }
+                    }
                 }
+
+                is Resource.Error -> {
+                    Toast.makeText(
+                        this@MainActivity,
+                        weatherState.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                is Resource.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(CulturedWhite),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator(color = Cloudy50)
+                    }
+                }
+
+                else -> {}
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherApp()
 }
--- app/src/main/java/com/lacorp/weather_app/presentation/WeatherViewModel.kt
+++ app/src/main/java/com/lacorp/weather_app/presentation/WeatherViewModel.kt
@@ -1,14 +1,124 @@
 package com.lacorp.weather_app.presentation
 
+import android.annotation.SuppressLint
+import android.location.Address
+import android.location.Geocoder
+import android.os.Build
 import androidx.lifecycle.ViewModel
-import com.lacorp.weather_app.domain.repository.IWeatherRepository
+import androidx.lifecycle.viewModelScope
+import com.lacorp.weather_app.common.Resource
+import com.lacorp.weather_app.data.mapper.toWeatherData
 import com.lacorp.weather_app.domain.location.ILocationClient
+import com.lacorp.weather_app.domain.model.Weather
+import com.lacorp.weather_app.domain.repository.IWeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.TimeZone
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 @HiltViewModel
 class WeatherViewModel @Inject constructor(
     private val repository: IWeatherRepository,
-    private val locationClient: ILocationClient
+    private val locationClient: ILocationClient,
 ) : ViewModel() {
+
+    private val _weather: MutableStateFlow<Resource<Weather>> = MutableStateFlow(Resource.Empty)
+    val weather = _weather.asStateFlow()
+
+    private val _location: MutableStateFlow<String> = MutableStateFlow("")
+    val location = _location.asStateFlow()
+
+    private val _datetime: MutableStateFlow<String> = MutableStateFlow("")
+    val datetime = _datetime.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            getDatetime().collect { newDatetime ->
+                _datetime.value = newDatetime
+            }
+        }
+    }
+
+    fun loadWeather(geocoder: Geocoder) {
+        viewModelScope.launch {
+            _weather.value = Resource.Loading
+            locationClient.getCurrentLocation()?.let { location ->
+                when (val hourly =
+                    repository.getWeatherHourlyData(location.latitude, location.longitude)) {
+                    is Resource.Success -> {
+                        when (val daily = repository.getWeatherDailyData(
+                            location.latitude,
+                            location.longitude,
+                            TimeZone.getDefault().id
+                        )) {
+                            is Resource.Success -> {
+                                val lastLocation =
+                                    getAddress(geocoder, location.latitude, location.longitude)
+
+                                _location.value =
+                                    lastLocation.subLocality ?: lastLocation.adminArea
+
+                                val weather = Weather(hourly.data, daily.data)
+                                _weather.value = Resource.Success(weather.toWeatherData())
+                            }
+
+                            is Resource.Error -> {
+                                _weather.value = Resource.Error(daily.message)
+                            }
+
+                            else -> Unit
+                        }
+                    }
+
+                    is Resource.Error -> {
+                        _weather.value = Resource.Error(hourly.message)
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private suspend fun getAddress(geocoder: Geocoder, lat: Double, lon: Double): Address {
+        return suspendCoroutine {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                geocoder.getFromLocation(lat, lon, 1) { address ->
+                    if (address.size != 0) {
+                        it.resume(address[0])
+                    }
+                }
+            } else {
+                val address = geocoder.getFromLocation(lat, lon, 1)
+                if (address != null && address.size != 0) {
+                    it.resume(address[0])
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDatetime(): Flow<String> = flow {
+        while (true) {
+            val formatter = SimpleDateFormat("EEEE , MMM yy, HH:mm a")
+            val time = Calendar.getInstance().time
+            val result = formatter.format(time)
+                .replace("am", "AM")
+                .replace("pm", "PM")
+            emit(result)
+            delay(10L)
+        }
+    }.flowOn(Dispatchers.IO)
 }
