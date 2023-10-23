8a261e671ad691f0e6c1bb35f475bdb254f22c5c
Lana <59499676+Lanzkuy@users.noreply.github.com>
2023-10-23 12:03:20 +0700

    remove WeatherState

--- app/src/main/java/com/lacorp/weather_app/presentation/WeatherState.kt
+++
@@ -1,9 +0,0 @@
-package com.lacorp.weather_app.presentation
-
-import com.lacorp.weather_app.domain.model.Weather
-
-data class WeatherState (
-    val weatherData: Weather? = null,
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
