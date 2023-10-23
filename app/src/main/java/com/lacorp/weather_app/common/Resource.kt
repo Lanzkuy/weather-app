fb9c71791490e79712d28e8e8bba402f1415a795
Lana <59499676+Lanzkuy@users.noreply.github.com>
2023-10-23 11:52:24 +0700

    update Resource class

--- app/src/main/java/com/lacorp/weather_app/common/Resource.kt
+++ app/src/main/java/com/lacorp/weather_app/common/Resource.kt
@@ -1,7 +1,9 @@
 package com.lacorp.weather_app.common
 
-sealed class Resource<out T>() {
-    class Success<T>(data: T?) : Resource<T>()
+sealed class Resource<out T> {
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Error(val message: String) : Resource<Nothing>()
     object Loading : Resource<Nothing>()
-    class Error(message: String) : Resource<Nothing>()
+
+    object Empty : Resource<Nothing>()
 }
